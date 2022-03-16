254a988c4e721119bb1c90e030a757e42ce88866
harshmishra2 <mishra.harshmishra727@gmail.com>
2022-03-16 18:26:07 +0000

    changes made

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    // p = document.getElementById("principal").value;
+
+//getting all values to get started with calculation using id tags of html elements
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);
+    var interest = principal * years * rate /100;
+
+//Creating interest text
+ document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
 }
-        
+
+//function to update the rate value
+function getSliderValue(){
+    document.getElementById("ratespan").innerHTML = document.getElementById("rate").value;
+}
