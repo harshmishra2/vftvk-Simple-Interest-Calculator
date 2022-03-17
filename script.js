a134a245a6fdf5ba1b25cfa582e1ba1af627e14d
harshmishra2 <mishra.harshmishra727@gmail.com>
2022-03-17 06:10:16 +0000

    final changes

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+    
+    if(principal <=0){
+        alert("Enter a positive number")
+        document.getElementById("principal").value=""
+         document.getElementById("principal").focus();
+    }else{
     var year = new Date().getFullYear()+parseInt(years);
     var interest = principal * years * rate /100;
 
 //Creating interest text
  document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+}
 }
 
 //function to update the rate value
